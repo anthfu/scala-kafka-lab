ff0be3f750f1b3f340db75e309c3acca7a64261b
Anthony Hrvojic <1017633+anthfu@users.noreply.github.com>
2021-06-21 09:45:03 -0400

    Initial client apps. Scalafmt support.

---
+++ zio-consumer/src/main/scala/com/anthfu/kafka/zio/consumer/ZioConsumer.scala
@@ -0,0 +1,7 @@
+package com.anthfu.kafka.zio.consumer
+
+import zio._
+
+object ZioConsumer extends App {
+  override def run(args: List[String]): URIO[ZEnv, ExitCode] = ???
+}
---
+++ zio-producer/src/main/scala/com/anthfu/kafka/zio/producer/ZioProducer.scala
@@ -0,0 +1,7 @@
+package com.anthfu.kafka.zio.producer
+
+import zio._
+
+object ZioProducer extends App {
+  override def run(args: List[String]): URIO[ZEnv, ExitCode] = ???
+}
