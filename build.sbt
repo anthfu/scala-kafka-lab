a91485cfa543853f3d8dbeedfc0f0cce07ca3ead
Anthony Hrvojic <1017633+anthfu@users.noreply.github.com>
2021-06-22 16:29:45 -0400

    ZIO producer WIP.

--- build.sbt
+++ build.sbt
@@ -2,14 +2,12 @@ ThisBuild / organization := "com.anthfu"
 ThisBuild / scalaVersion := "2.13.6"
 ThisBuild / version := "1.0.0-SNAPSHOT"
 
-lazy val kafkaVersion = "2.8.0"
 lazy val zioVersion = "1.0.9"
 
 lazy val commonSettings = Seq(
   libraryDependencies ++= Seq(
     "dev.zio" %% "zio-kafka" % "0.15.0",
-    "dev.zio" %% "zio-streams" % zioVersion,
-    "org.apache.kafka" % "kafka-clients" % kafkaVersion
+    "dev.zio" %% "zio-streams" % zioVersion
   )
 )
 
--- zio-producer/src/main/scala/com/anthfu/kafka/zio/producer/ZioProducer.scala
+++ zio-producer/src/main/scala/com/anthfu/kafka/zio/producer/ZioProducer.scala
@@ -1,7 +1,30 @@
 package com.anthfu.kafka.zio.producer
 
+import org.apache.kafka.clients.producer.ProducerRecord
 import zio._
+import zio.console.putStrLn
+import zio.kafka.producer.{Producer, ProducerSettings}
+import zio.kafka.serde.Serde
+import zio.stream.ZStream
+
+import java.util.UUID
 
 object ZioProducer extends App {
-  override def run(args: List[String]): URIO[ZEnv, ExitCode] = ???
+  type ProducerEnv = ZEnv with Producer[Any, UUID, String]
+
+  override def run(args: List[String]): URIO[ZEnv, ExitCode] = {
+    val settings = ProducerSettings(List("localhost:9092"))
+    val producer = ZLayer.fromManaged(Producer.make(settings, Serde.uuid, Serde.string))
+    zio.provideSomeLayer(producer).exitCode
+  }
+
+  private def zio: ZIO[ProducerEnv, Throwable, Unit] =
+    ZStream
+      .fromIterable(0 to 1000)
+      .map(n => new ProducerRecord("test-topic", UUID.randomUUID(), n.toString))
+      .mapM { rec =>
+        putStrLn(s"Sent: ${rec.value}") *>
+          Producer.produce[Any, UUID, String](rec)
+      }
+      .runDrain
 }
