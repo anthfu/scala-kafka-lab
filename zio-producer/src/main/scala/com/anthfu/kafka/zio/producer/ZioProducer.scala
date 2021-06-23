4d8040f4ad4bf355a7e7cd77fdd7f773c3885821
Anthony Hrvojic <1017633+anthfu@users.noreply.github.com>
2021-06-23 09:06:45 -0400

    Minor simplification.

--- zio-producer/src/main/scala/com/anthfu/kafka/zio/producer/ZioProducer.scala
+++ zio-producer/src/main/scala/com/anthfu/kafka/zio/producer/ZioProducer.scala
@@ -10,15 +10,13 @@ import zio.stream.ZStream
 import java.util.UUID
 
 object ZioProducer extends App {
-  type ProducerEnv = ZEnv with Producer[Any, UUID, String]
-
   override def run(args: List[String]): URIO[ZEnv, ExitCode] = {
     val settings = ProducerSettings(List("localhost:9092"))
     val producer = ZLayer.fromManaged(Producer.make[Any, UUID, String](settings, Serde.uuid, Serde.string))
-    zio.provideSomeLayer[ZEnv](producer).exitCode
+    stream.provideSomeLayer[ZEnv](producer).exitCode
   }
 
-  private def zio: ZIO[ProducerEnv, Throwable, Unit] =
+  private lazy val stream =
     ZStream
       .fromIterable(0 to 1000)
       .map(n => new ProducerRecord("test-topic", UUID.randomUUID(), n.toString))
