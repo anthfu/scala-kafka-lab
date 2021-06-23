e66d3edfa23609135739a0c20d3710a161c901ad
Anthony Hrvojic <1017633+anthfu@users.noreply.github.com>
2021-06-23 14:34:03 -0400

    Introduced `zio-config` for Kafka configuration.

--- zio-producer/src/main/scala/com/anthfu/kafka/zio/producer/ZioProducer.scala
+++ zio-producer/src/main/scala/com/anthfu/kafka/zio/producer/ZioProducer.scala
@@ -2,30 +2,46 @@ package com.anthfu.kafka.zio.producer
 
 import org.apache.kafka.clients.producer.ProducerRecord
 import zio._
+import zio.config.ConfigDescriptor._
+import zio.config._
+import zio.config.yaml.YamlConfig
 import zio.console.putStrLn
 import zio.kafka.producer.{Producer, ProducerSettings}
 import zio.kafka.serde.Serde
 import zio.stream.ZStream
 
+import java.nio.file.Path
 import java.util.UUID
 
 object ZioProducer extends App {
-  type ProducerEnv = ZEnv with Producer[Any, UUID, String]
+  case class AppConfig(topic: String, bootstrapServer: String)
 
-  override def run(args: List[String]): ZIO[ZEnv, Nothing, ExitCode] =
-    stream.provideSomeLayer[ZEnv](producerLayer).exitCode
+  type AppEnv = ZEnv with Has[AppConfig] with Producer[Any, UUID, String]
 
-  private def stream: ZIO[ProducerEnv, Throwable, Unit] =
+  override def run(args: List[String]): ZIO[ZEnv, Nothing, ExitCode] = {
+    val appLayer = configLayer ++ producerLayer
+    stream.provideLayer(appLayer).exitCode
+  }
+
+  private def stream: ZIO[AppEnv, Throwable, Unit] =
     ZStream
       .fromIterable(0 to 1000)
-      .map(n => new ProducerRecord("test-topic", UUID.randomUUID(), n.toString))
-      .mapM { rec =>
-        putStrLn(s"Sent: ${rec.value}") *>
-          Producer.produce[Any, UUID, String](rec)
+      .mapM { n =>
+        for {
+          conf <- getConfig[AppConfig]
+          rec   = new ProducerRecord(conf.topic, UUID.randomUUID(), n.toString)
+          _    <- Producer.produce[Any, UUID, String](rec)
+          _    <- putStrLn(s"Sent: ${rec.value}")
+        } yield ()
       }
       .runDrain
 
-  private def producerLayer: ZLayer[ZEnv, Throwable, Producer[Any, UUID, String]] = {
+  private def configLayer: ZLayer[ZEnv, Throwable, Has[AppConfig]] = {
+    val descriptor = (string("app/topic") |@| string("app/bootstrap_server"))(AppConfig.apply, AppConfig.unapply)
+    YamlConfig.fromPath(Path.of("src/main/resources/application.yml"), descriptor)
+  }
+
+  private def producerLayer: ZLayer[ZEnv with Has[AppConfig], Throwable, Producer[Any, UUID, String]] = {
     val settings = ProducerSettings(List("localhost:9092"))
     ZLayer.fromManaged(Producer.make[Any, UUID, String](settings, Serde.uuid, Serde.string))
   }
