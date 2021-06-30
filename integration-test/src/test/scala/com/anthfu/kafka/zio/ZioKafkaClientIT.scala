027a2eb0d77c7abaa6aebd42bf9aeb9476626110
Anthony Hrvojic <1017633+anthfu@users.noreply.github.com>
2021-06-30 16:15:25 -0400

    Integration test scaffolding.

--- integration-test/src/test/scala/com/anthfu/kafka/zio/ZioKafkaClientIT.scala
+++ integration-test/src/test/scala/com/anthfu/kafka/zio/ZioKafkaClientIT.scala
@@ -1,7 +1,15 @@
 package com.anthfu.kafka.zio
 
+import com.dimafeng.testcontainers.munit.TestContainerForAll
+import com.dimafeng.testcontainers.{ContainerDef, KafkaContainer}
 import munit.FunSuite
 
-class ZioKafkaClientIT extends FunSuite {
-  // TODO
+class ZioKafkaClientIT extends FunSuite with TestContainerForAll {
+  override val containerDef: ContainerDef = KafkaContainer.Def()
+
+  test("Send and receive messages") {
+    withContainers { kafkaContainer =>
+      // TODO
+    }
+  }
 }
