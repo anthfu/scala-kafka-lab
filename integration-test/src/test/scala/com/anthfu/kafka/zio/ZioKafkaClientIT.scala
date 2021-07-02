72b09d4dc97b18da3f14813c41f64512ad0a6c92
Anthony Hrvojic <1017633+anthfu@users.noreply.github.com>
2021-07-02 13:43:23 -0400

    Integration test WIP.

--- integration-test/src/test/scala/com/anthfu/kafka/zio/ZioKafkaClientIT.scala
+++ integration-test/src/test/scala/com/anthfu/kafka/zio/ZioKafkaClientIT.scala
@@ -4,31 +4,50 @@ import com.dimafeng.testcontainers.lifecycle.and
 import com.dimafeng.testcontainers.munit.TestContainersForAll
 import com.dimafeng.testcontainers.{GenericContainer, KafkaContainer}
 import munit.FunSuite
+import org.slf4j.LoggerFactory
+import org.testcontainers.containers.Network
+import org.testcontainers.containers.output.Slf4jLogConsumer
 import org.testcontainers.containers.wait.strategy.Wait
 
 class ZioKafkaClientIT extends FunSuite with TestContainersForAll {
+  private val logger = LoggerFactory.getLogger(getClass)
+
   override type Containers = KafkaContainer and ConsumerContainer and ProducerContainer
 
   override def startContainers(): Containers = {
+    val network = Network.newNetwork()
+
     val kafkaContainer = KafkaContainer.Def("6.2.0").start()
-    val consumerContainer = ConsumerContainer.Def(port = 8080).start()
-    val producerContainer = ProducerContainer.Def(port = 8080).start()
+    kafkaContainer.container.withNetwork(network)
+    kafkaContainer.container.withNetworkAliases("kafka")
+
+    val consumerContainer = ConsumerContainer.Def().start()
+    consumerContainer.container.withNetwork(network)
+    consumerContainer.container.withLogConsumer(new Slf4jLogConsumer(logger).withPrefix("zio-consumer"))
+    consumerContainer.container.dependsOn(kafkaContainer)
+
+    val producerContainer = ProducerContainer.Def().start()
+    producerContainer.container.withNetwork(network)
+    producerContainer.container.withLogConsumer(new Slf4jLogConsumer(logger).withPrefix("zio-producer"))
+    producerContainer.container.dependsOn(consumerContainer)
+
     kafkaContainer and consumerContainer and producerContainer
   }
 
   test("Send and receive messages") {
-    withContainers { case kafkaContainer and consumerContainer and producerContainer =>
-      // TODO
+    withContainers { case _ and consumerContainer and producerContainer =>
+      assert(producerContainer.container.getLogs.contains("value: 1000"))
+      assert(consumerContainer.container.getLogs.contains("value: 1000"))
     }
   }
 }
 
 class ConsumerContainer(underlying: GenericContainer) extends GenericContainer(underlying)
 object ConsumerContainer {
-  case class Def(port: Int) extends GenericContainer.Def[ConsumerContainer](
+  case class Def() extends GenericContainer.Def[ConsumerContainer](
     new ConsumerContainer(GenericContainer(
       dockerImage = "zio-consumer:1.0.0-SNAPSHOT",
-      exposedPorts = Seq(port),
+      exposedPorts = Seq(8080),
       waitStrategy = Wait.forHttp("/")
     ))
   )
@@ -36,10 +55,10 @@ object ConsumerContainer {
 
 class ProducerContainer(underlying: GenericContainer) extends GenericContainer(underlying)
 object ProducerContainer {
-  case class Def(port: Int) extends GenericContainer.Def[ProducerContainer](
+  case class Def() extends GenericContainer.Def[ProducerContainer](
     new ProducerContainer(GenericContainer(
       dockerImage = "zio-producer:1.0.0-SNAPSHOT",
-      exposedPorts = Seq(port),
+      exposedPorts = Seq(8080),
       waitStrategy = Wait.forHttp("/")
     ))
   )
